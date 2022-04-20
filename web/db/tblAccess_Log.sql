b7b25951ca104d8190852c0e3bf8935332ab18b9
gbriellek <gabrielle.khouw@student.uts.edu.au>
2022-04-20 15:45:07 +1000

    added databases and sql files

---
+++ web/db/tblAccess_Log.sql
@@ -0,0 +1,17 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Gabrielle K
+ * Created: 20 Apr. 2022
+ */
+
+CREATE TABLE tblAccess_Log (
+    Access_Log_ID int NOT NULL,
+    User_ID int NOT NULL,
+    Access_Date_Time TIMESTAMP NOT NULL,
+    Event VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Access_Log_ID),
+    FOREIGN KEY (User_ID) REFERENCES tblUser(User_ID)
+);
---
+++ web/db/tblBilling_Address.sql
@@ -0,0 +1,20 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Gabrielle K
+ * Created: 20 Apr. 2022
+ */
+
+CREATE TABLE tblBilling_Address(
+    Billing_Address_ID int NOT NULL,
+    Address_ID int NOT NULL,
+    User_ID int,
+    Order_ID int,
+    Purpose VARCHAR(5) NOT NULL,
+    PRIMARY KEY (Billing_Address_ID),
+    FOREIGN KEY (Address_ID) REFERENCES tblAddress(Address_ID),
+    FOREIGN KEY (User_ID) REFERENCES tblUser(User_ID),
+    FOREIGN KEY (Order_ID) REFERENCES tblOrder(Order_ID)
+)
